e8c7f1462b3dcbed007e0285dbce3f69d0c40365
rehmpke <rehmpke@gmail.com>
2020-04-25 22:53:37 -0500

    Remove data-src and clean up LazyLoad

--- assets/js/src/lazyload.js
+++ assets/js/src/lazyload.js
@@ -1,31 +1,14 @@
 function lazyLoad() {
-  //   const lazyLoadImages = document.querySelectorAll('img[data-src]');
-  
-  //   lazyLoadImages.forEach((img) => {
-  //     img.src = img.dataset.src;
-  //     img.onload = () => img.removeAttribute('data-src');
-  //   });
-  // }
-  //
-  // create config object: rootMargin and threshold
-  // are two properties exposed by the interface
+
   const config = {
     rootMargin: '0px 0px 50px 0px',
     threshold: 0
   };
   
-  // register the config object with an instance
-  // of intersectionObserver
   let observer = new IntersectionObserver(function(entries, self) {
-    // iterate over each entry
     entries.forEach(entry => {
-      // process just the images that are intersecting.
-      // isIntersecting is a property exposed by the interface
       if(entry.isIntersecting) {
-        // custom function that copies the path to the img
-        // from data-src to src
         preloadImage(entry.target);
-        // the image is now in place, stop watching
         self.unobserve(entry.target);
       }
     });
@@ -40,8 +23,11 @@ function lazyLoad() {
     const src = img.getAttribute('data-src');
     if (!src) { return; }
     img.src = src;
+    img.onload = function() {
+      img.removeAttribute('data-src');
+    };
   }
   
-  }
+}
   
-  export default lazyLoad;
+export default lazyLoad;
