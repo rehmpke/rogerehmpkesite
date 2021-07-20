c024da2dd66e0b00d36caef2f6ffd0bfdd49236b
rehmpke <rehmpke@gmail.com>
2021-07-20 17:09:44 -0500

    Remove angle brackets around endpoint

--- assets/js/src/contact.js
+++ assets/js/src/contact.js
@@ -8,7 +8,7 @@ form.addEventListener("submit", (event) => {
 
         // Use your API endpoint URL you copied from the previous step
   const endpoint =
-    "<https://f1gpiut934.execute-api.us-east-1.amazonaws.com/default/SendContactEmail>";
+    "https://f1gpiut934.execute-api.us-east-1.amazonaws.com/default/SendContactEmail";
   // We use JSON.stringify here so the data can be sent as a string via HTTP
         const body = JSON.stringify({
     senderName: name.value,
