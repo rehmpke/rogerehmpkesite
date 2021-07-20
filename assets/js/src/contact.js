13f7d87eed47569e9c8adb4c1f0c92c90cc588a0
rehmpke <rehmpke@gmail.com>
2021-07-20 16:59:47 -0500

    Add contact aws to lamda

--- assets/js/src/contact.js
+++ assets/js/src/contact.js
@@ -1,15 +1,39 @@
 function contactMe() {
-const form = document.querySelector('form')
-form.addEventListener('submit', event => {
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
   // prevent the form submit from refreshing the page
-  event.preventDefault()
+  event.preventDefault();
 
-  const { name, email, message } = event.target
-  console.log('Name: ', name.value)
-  console.log('Email Address: ', email.value)
-  console.log('Message: ', message.value)
+  const { name, email, message } = event.target;
 
-})
+        // Use your API endpoint URL you copied from the previous step
+  const endpoint =
+    "<https://f1gpiut934.execute-api.us-east-1.amazonaws.com/default/SendContactEmail>";
+  // We use JSON.stringify here so the data can be sent as a string via HTTP
+        const body = JSON.stringify({
+    senderName: name.value,
+    senderEmail: email.value,
+    message: message.value
+  });
+  const requestOptions = {
+    method: "POST",
+    body
+  };
+
+  fetch(endpoint, requestOptions)
+    .then((response) => {
+      if (!response.ok) throw new Error("Error in fetch");
+      return response.json();
+    })
+    .then((response) => {
+      document.getElementById("result-text").innerText =
+        "Email sent successfully!";
+    })
+    .catch((error) => {
+      document.getElementById("result-text").innerText =
+        "An unkown error occured.";
+    });
+});
 }
 
 export default contactMe;
