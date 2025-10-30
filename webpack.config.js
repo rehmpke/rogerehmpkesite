2d80b82849f69ab1a4595597453a20b14c89519f
Roger Ehmpke <rehmpke@gmail.com>
2025-10-29 21:22:24 -0500

    Correct legacy javascript message in browser

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,64 @@
-const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
+// webpack.config.js
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const MyHashWebpackPlugin = require('./buildtools/myHashWebpackPlugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
-  watch: true,
+  mode: isProd ? 'production' : 'development',
+  target: 'browserslist',
+  watch: !isProd,
+  devtool: isProd ? false : 'source-map',
+
   entry: './assets/js/src/index.js',
+
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, './assets/js/')
   },
-  stats: {
-    children: true
-  },
+
+  stats: { children: true },
+
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: [
-          path.resolve(__dirname, 'node_modules')
-        ],
+        test: /\.m?js$/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            cacheDirectory: true, // Cache the babel compilation // Cached files are stored & gzipped in `/node_modules/.cache/babel-loader/`
-            // Set a custom `cacheIdentifier: '<my_custom_cache_identifier>',` SHOULD you need to manually bust the babel-loader cache.
-          },
-        },
+          options: { cacheDirectory: true }
+        }
       },
       {
-        test: [/.scss$/],
+        test: /\.scss$/,
         use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {  // Do not let css-loader resolve `url()`s
-              url: false, // Will throw errors when trying to resolve url() for local assets if set to default (true)
-            },
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { url: false, sourceMap: !isProd }
           },
-          'postcss-loader',
-          'sass-loader',
-        ]
-    }
-    ]
-  },
-  resolve: {
-    extensions: ['.json', '.js', '.jsx', '.scss']
-  },
-  plugins: [
-    new MyHashWebpackPlugin({path: '../_data/', fileName: 'hash.yml'}),
-    new MiniCssExtractPlugin({
-        filename: '../css/style.css'
-    }),
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        postcss: [
-          autoprefixer()
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: !isProd }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass-embedded'),
+              sourceMap: !isProd,
+              sassOptions: { quietDeps: true }
+            }
+          }
         ]
       }
-    })
+    ]
+  },
+
+  resolve: { extensions: ['.mjs', '.js', '.json', '.scss'] },
+
+  plugins: [
+    new MyHashWebpackPlugin({ path: '../_data/', fileName: 'hash.yml' }),
+    new MiniCssExtractPlugin({ filename: '../css/style.css' })
   ]
 };
